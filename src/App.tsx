a4ee4706d065e8df68433e9688266d021dc8cbfd
hotequil <hotequil@protonmail.com>
2022-07-06 21:58:25 -0300

    feat: done layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import './App.css';
+import styles from "./App.module.css"
 
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img className={styles.logo} src="./logo192.png" alt="Dodone logo" />
+        <h1 className={styles.title}>Dodone</h1>
+      </header>
+      <main className={styles.main}>Test</main>
+      <footer className={styles.footer}>Developed by hotequil</footer>
+    </div>
+  )
 }
 
 export default App;
