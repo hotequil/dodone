e8028f7e00b0794eb692b34cb9d672813b548834
hotequil <hotequil@protonmail.com>
2022-07-10 21:09:20 -0300

    feat: done Button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,6 @@
+import styles from "./Button.module.css"
+import { BaseComponent } from "../../shared/types/Generic";
+
+export const Button = ({ children, ...props }: BaseComponent) => (
+  <button {...props} className={styles.button}>{children}</button>
+)
