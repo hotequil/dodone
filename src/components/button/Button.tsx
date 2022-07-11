80592fbd55b8e9d7304e744c2179b60961b19fa6
hotequil <hotequil@protonmail.com>
2022-07-11 19:40:39 -0300

    feat: added custom class in Button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,5 +2,5 @@ import styles from "./Button.module.css"
 import { BaseComponent } from "../../shared/types/Generic";
 
 export const Button = ({ children, ...props }: BaseComponent) => (
-  <button {...props} className={styles.button}>{children}</button>
+  <button {...props} className={`${styles.button} ${props.className || ''}`}>{children}</button>
 )
