adf8afea67267e561a87b11a14db546a1e3e9905
hotequil <hotequil@protonmail.com>
2022-07-11 20:47:12 -0300

    feat: removed translate from my surname

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ export const App = () => {
     <div className={styles.container}>
       <header className={styles.header}>
         <img className={styles.logo} src="./logo192.png" alt="Dodone logo" />
-        <h1 className={styles.title}>Dodone</h1>
+        <h1 className={styles.title} translate="no">Dodone</h1>
       </header>
       <main className={styles.main}>
         <TasksPage />
       </main>
-      <footer className={styles.footer}>Developed by hotequil</footer>
+      <footer className={styles.footer}>Developed by <span translate="no">hotequil</span></footer>
     </div>
   )
 }
